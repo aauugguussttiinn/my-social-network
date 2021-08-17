5dc7ff7660fbc906de1a99212d849f7d5d9bda6e
Augustin <augustin.berne.pro@gmail.com>
2021-08-17 12:40:29 +0200

    Navbar design is done

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,16 +3,22 @@ import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div className="navigation">
-      <NavLink exact to="/" activeClassName="nav-active">
-        Accueil
-      </NavLink>
-      <NavLink exact to="/about" activeClassName="nav-active">
-        About
-      </NavLink>
-      <NavLink exact to="/works" activeClassName="nav-active">
-        Works
-      </NavLink>
+    <div className="navigation d-flex align-items-center">
+      <div className="nav-left col-6">
+        <NavLink exact to="/" activeClassName="nav-active">
+          Accueil
+        </NavLink>
+        <NavLink exact to="/about" activeClassName="nav-active">
+          About
+        </NavLink>
+        <NavLink exact to="/works" activeClassName="nav-active">
+          Works
+        </NavLink>
+      </div>
+      <div className="nav-right col-6 d-flex justify-content-end">
+        <button className="btn btn-primary mx-2">Sign In</button>
+        <button className="btn btn-outline-primary mx-2">Log In</button>
+      </div>
     </div>
   );
 };
