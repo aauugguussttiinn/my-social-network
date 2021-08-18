388bf7e7a9f2371c4946c579d7932ed2b3aa65f6
Augustin <augustin.berne.pro@gmail.com>
2021-08-18 12:11:04 +0200

    Changed rootreducer to userReducer

--- src/index.jsx
+++ src/index.jsx
@@ -6,10 +6,11 @@ import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import rootReducer from "redux/reducers"
+// import rootReducer from "redux/reducers"
+import userReducer from "redux/reducers/userReducer";
 
 const store = createStore(
-  rootReducer,
+  userReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
 
