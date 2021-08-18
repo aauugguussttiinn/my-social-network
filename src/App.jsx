f45b44a3b5c01527fc73e545b2f2067bd38bfb62
Augustin <augustin.berne.pro@gmail.com>
2021-08-18 22:20:56 +0200

    About to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,49 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import Home from 'pages/Home/Home';
 import Register from 'pages/Register/Register';
 import Login from 'pages/Login/Login';
 import Profile from 'pages/Profile/Profile';
 import NotFound from 'pages/NotFound/NotFound';
-import loginUserWithCookie from 'auth/Cookies';
+import Cookies from "js-cookie";
+// import loginUserWithCookie from 'auth/Cookies';
 import { useEffect } from 'react';
 
 const App = () => {
 
   const currentUser = useSelector((state) => state.user);
+  const dispatch = useDispatch()
+
+  const loginUserWithCookie = async(dispatch) => {
+    const token = Cookies.get('token')
+
+    const cookiesConfig = {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    };
+  
+    const response = await fetch(`http://localhost:1337/users/me`, cookiesConfig);
+    const cookieData = await response.json();
+    console.log(cookieData);
+    if (cookieData) {
+      return true;
+    } else {
+      return false;
+    }
+
+    // const cookieLoginParams = {
+    //   identifier: ,
+    //   password: ,
+    // }
+    // await dispatch(loginUser(userData))
+  };
 
   const checkAuth = () => {
-    if (currentUser) {
+    if (currentUser || loginUserWithCookie() === true) {
       // console.log('User is logged in');
       return true;
     } else {
