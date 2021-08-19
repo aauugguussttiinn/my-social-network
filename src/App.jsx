07b0e8e45676c10ee95f29268729f64905f92db3
Augustin <augustin.berne.pro@gmail.com>
2021-08-19 12:13:23 +0200

    Loader working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux"
 
@@ -14,7 +14,15 @@ import Cookies from "js-cookie";
 const App = () => {
 
   const currentUser = useSelector((state) => state.user);
-  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [isAuthTrue, setIsAuthTrue] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    checkAuth().then(res => {
+      setIsAuthTrue(res);
+      setLoading(false);
+    })
+  });
 
   const loginUserWithCookie = async() => {
     const token = Cookies.get('token')
@@ -29,14 +37,9 @@ const App = () => {
   
     const response = await fetch(`http://localhost:1337/users/me`, cookiesConfig)
     const cookieData = await response.json();
-    // console.log(cookieData)
-    // console.log('test')
-    // console.log(cookieData);
     if (!cookieData.error) {
-      // console.log("it returns true")
       return true;
     } else {
-      // console.log("it returns false")
       return false;
     }
 
@@ -44,34 +47,21 @@ const App = () => {
 
   const checkAuth = async() => {
     const a = await (loginUserWithCookie());
-    // console.log(a);
-    // console.log(loginUserWithCookie())
     if (currentUser || a === true) {
-      // console.log('User is logged in');
-      setIsAuthorized(true);
-      // console.log(isAuthorized);
       return true;
     } else {
-      // console.log('User is NOT logged in');
-      // console.log(isAuthorized);
       return false;
     }
   }
-  // console.log(`checkAuth ${checkAuth()}`)
-  // console.log(`$$$$$$$$$$ ${isAuthorized}`)
-  // console.log("aaaaaaaa");
-  // console.log(checkAuth());
-  // console.log("aaaaaaaa");
+
   const PrivateRoute = ({ component: Component, ...rest }) => (
-    // checkAuth(),
-    // console.log(`teeeeeest ${isAuthorized}`),
-    <Route {...rest} render={props => (
-      isAuthorized ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to={{ pathname: '/login' }} />
-      )
-    )} />
+      <>
+        { loading ? (
+          <div>blabla</div>
+        ) : ( 
+          < Route {...rest} render={ props => ( isAuthTrue ? ( <Component {...props} />) : (  <Redirect to={{ pathname: '/login' }} /> ) )} />)
+        }
+      </>
   );
 
   return (
