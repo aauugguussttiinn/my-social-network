127a40a452876d61befd1e63993a1b58b637f90a
Augustin <augustin.berne.pro@gmail.com>
2021-08-19 22:27:24 +0200

    Edit profile redirection is done

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,16 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import { useDispatch, useSelector } from 'react-redux';
-import UserProfile from 'components/UserProfile/UserProfile';
 import { setUser } from 'redux/actions/userActions';
+
+import UserProfile from 'components/UserProfile/UserProfile';
+import Loader from 'components/Loader/Loader';
+
 
 
 const Profile = () => {
 
   const userProfile = useSelector((state) => state.userReducer);
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
 
   const getUserProfile = async() => {
+    setLoading(true)
     const token = Cookies.get('token')
   
     const cookiesConfig = {
@@ -23,22 +28,37 @@ const Profile = () => {
     };
 
     await dispatch(setUser(cookiesConfig))
-
+    setLoading(!loading)
   };
 
   useEffect(() => {
     getUserProfile();
     },
-    []
+    [ userProfile.changed === false ]
   );
 
   return (
     <div className="profile container">
+      { loading ? (
+        <Loader type='spin' color='#454545' />
+      ) : ( 
+        ( userProfile.changed === false ? (
+          <>
+            <h1>Welcome to your profile page, { userProfile.username } !</h1>
+            <div className="my-profile-container">
+              <UserProfile username={ userProfile.username } email={ userProfile.email } />
+            </div>
+          </>
+           ) : (
+           <Redirect to={{ pathname: '/profile' }} />
+           )
+        ))
+      }
       {/* <h1>Welcome to your profile page, { userProfile.username } !</h1>
         <div className="my-profile-container">
           <UserProfile username={ userProfile.username } email={ userProfile.email } />
         </div> */}
-      { userProfile.changed === false ? (
+      {/* { userProfile.changed === false ? (
         <>
           <h1>Welcome to your profile page, { userProfile.username } !</h1>
           <div className="my-profile-container">
@@ -47,7 +67,7 @@ const Profile = () => {
         </>
       ) : (
         <Redirect to={{ pathname: '/' }} />
-      )}
+      )} */}
     </div>
   );
 };
